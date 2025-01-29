307c6236fc952f9c66426ddc805919dff041ec92
laludonisetiawan <laludonisetiawan@gmail.com>
2025-01-29 12:05:08 +0700

    Add project files.

---
+++ CRUD_SQL_SERVER/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUD_SQL_SERVER.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_SQL_SERVER.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+
+        }
+        // inisialisasi tabel db
+        public DbSet<Employee> Employees { get; set; }
+          
+    }
+}
---
+++ CRUD_SQL_SERVER/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿ namespace CRUD_SQL_SERVER.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
